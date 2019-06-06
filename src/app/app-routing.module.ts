65d3262648ccbebcdbab75c21d8723326277fa75
Shane Smith <voodoogq@gmail.com>
2019-06-06 13:39:42 -0500

    Detail routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
+  { path: 'details/:id', component: HeroDetailComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 
